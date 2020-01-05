bb8a5e2f59546d2d3314db61e8c172bd71a43645
Babak Nikanjam <babak.nikanjam@gmail.com>
2020-01-04 17:49:01 -0800

    doc: main algorithm docstring

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,44 @@
-"""Distinct set of lines intersecting 3 or more points in 2-dimensional space
-input: a list of x, y coordinates in 2-dimensional space as an input, and
-returns a distinct list of lines that intersect 3 or more points from the input set.
+"""Group collinear Points and represent/return them as distinct lines in Cartesian coordinate system.
+A line in Cartesian coordinate system can be represented as y = slop * x + intercept
+
+Notes:
+    There could more than 3 collinear points which will be represented with a distinct line together.
+    Lines represented as a tuple of 2 elements (slop, intercept)
+    Input:  list of (x, y)
+    Output: list of (slop, intercept)
+
+    We chose `immutable` tuple data structure for (x, y) as this data is not to change.
+    We use functional programming per project requirement. (Rather alternative class-based solution)
+
+    Per project requirement the function must be imported to other parts of a main app.
+    It purposely raise a RuntimeError if run independently.
+
+    To avoid inherent floating point error we use built-in Decimal module.
+    Example:
+        number = 0.1 + 0.1 + 0.1
+        number = 0.30000000000000004
+
+        number = Decimal('0.1') + Decimal('0.1') + Decimal('0.1')
+        float(Decimal('0.3') = 0.3
+
+
+
+Please return your project in a zipped folder that contains your
+- code,
+- test cases, and a
+- ReadMe.
+
+Success Criteria:We will be assessing your
+- project's architecture,
+- execution, and
+- coding style.
+-  extensible, well organized, and
+- well documented project.
+
+Please take time to code to your highest professional standard.
+
+Constraints:Please refrain from using external libraries.
+Built-in Python libraries are fine.
 """
 
 from itertools import combinations
@@ -37,6 +75,9 @@ def get_lines(points: List[Tuple[Any, Any]]) -> List[Tuple[float, float]]:
     # Duplicates carry no meaningful information and increase complexity.
     points = list(set(points))
 
+    # itertools.combinations returns a generator therefore during the main algorithm for loop we only travers possible
+    # combinations `once` giving us O( (n * n-1 * n-2)/6 ) i.e. O(n^3) far from ideally a linear runtime complexity.
+    # The worst-case space complexity returning the results is O( n/3 )
     all_three_pnts_combinations = combinations(points, 3)
     lines_set = set()
     for cmb in all_three_pnts_combinations:
